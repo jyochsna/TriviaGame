dcc9160de33e1543f6568adbe51f60d52101afad
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-01-04 18:25:14 -0800

    some function is coded on js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+
+var questionBanks = [
+    { 
+        question: "What is the color of Sky? ",
+        answers: {
+            a: "red" ,
+            b: "black" ,
+            c: "pink",
+            d: "blue"
+        },
+        correctAnswer: "d"
+    },
+
+        {
+            question: "How many bones do human body have? ",
+            answers: {
+                a: 207 ,
+                b: 206 ,
+                c: 350 ,
+                d: 309
+            },
+            correctAnswer: "b"
+            
+        },
+    
+];
+var output = [];
+function builQuiz(questionBanks){
+    
+    var answers;
+
+for(var i = 0; i<questionBanks.length; i++){
+    answers = [];
+    for(letter in questionBanks[i].answers){
+        answers.push(
+            '<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questionBanks[i].answers[letter]
+				+ '</label>'
+        );
+    }
+    output.push('<div class="question">' + questionBanks[i].question + '</div>'
+    + '<div class="answers">' + answers.join('') + '</div>');
+    console.log(output);
+
+}
+// quizContainer.innerHTML = output.join("");
+}
+builQuiz(questionBanks);
+$("#quiz").append(output);
